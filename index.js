d185cd67c4c14d552eb44c6c9f6d9ba350cf2b19
Jon Schlinkert <dev@sellside.com>
2013-09-22 19:58:52 -0400

    move frep to root

--- index.js
+++
@@ -1,8 +0,0 @@
-/**!
- * frep
- * http://github.com/helpers/frep
- * Copyright (c) 2013, Jon Schlinkert, contributors
- * Licensed under the MIT license.
- */
-
-module.exports = require('./lib/frep');
